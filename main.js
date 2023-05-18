31779b302d55f25c4beee73db116fea4f657456c
Karen Penedo <penedok12@gmail.com>
2023-05-18 14:15:52 -0400

    adicionando logo e a pasta CSS e arquivo Header para estilização

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
 
-
- function cadastra(){
-  const btnEnviar = document.querySelector('.enviar')
-    btnEnviar.addEventListener('click', ()=>{
-        let cadastro = {
-            email : document.querySelector('.email-cadastrado').value,
-            senha : document.querySelector('.senha-cadastrada').value   
-        }
-          console.log(cadastro)
-
-    })
-
-    
- }
-
-
-cadastra()
